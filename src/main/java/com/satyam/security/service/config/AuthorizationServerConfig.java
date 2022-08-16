06895eb6d4a1474bc634933839feb2454058aed0
Satyam Kumar <skumar@texala.net>
2022-08-16 13:30:29 +0530

    change in oauth

--- src/main/java/com/satyam/security/service/config/AuthorizationServerConfig.java
+++ src/main/java/com/satyam/security/service/config/AuthorizationServerConfig.java
@@ -1,9 +1,15 @@
 package com.satyam.security.service.config;
 
+import java.util.Arrays;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -12,9 +18,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+/**
+ * 
+ * @author SKumar
+ *
+ */
 
 /**
  * 
@@ -25,33 +40,22 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-	static final String CLIEN_ID = "devglan-client";
-	static final String CLIENT_SECRET = "devglan-secret";
-	static final String GRANT_TYPE_PASSWORD = "password";
-	static final String AUTHORIZATION_CODE = "authorization_code";
-	static final String REFRESH_TOKEN = "refresh_token";
-	static final String IMPLICIT = "implicit";
-	static final String SCOPE_READ = "read";
-	static final String SCOPE_WRITE = "write";
-	static final String TRUST = "trust";
-	static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1 * 60 * 60;
-	static final int FREFRESH_TOKEN_VALIDITY_SECONDS = 6 * 60 * 60;
-	
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private DataSource dataSource;
+	@Autowired
+	private PasswordEncoder encoder;
 	@Autowired
 	private WebResponseExceptionTranslator<OAuth2Exception> loggingExceptionTranslator;
-	
+	@Resource(name = "userDetailsServiceImpl")
+	private UserDetailsService userDetailsService;
+
 	@Override
-	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
-		configurer.inMemory().withClient(CLIEN_ID).secret(passwordEncoder.encode(CLIENT_SECRET))
-				.authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT)
-				.scopes(SCOPE_READ, SCOPE_WRITE, TRUST).accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).
-				refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.jdbc(dataSource).passwordEncoder(encoder);
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()")
@@ -60,20 +64,39 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-
-		endpoints.authenticationManager(authenticationManager)// .tokenEnhancer(tokenEnhancerChain)
-				.tokenStore(tokenStore()).exceptionTranslator(loggingExceptionTranslator);
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer()));
+		endpoints.tokenStore(tokenStore()).//accessTokenConverter(accessTokenConverter()).
+		tokenServices(defaultTokenServices()).
+		tokenEnhancer(tokenEnhancerChain).
+				authenticationManager(authenticationManager);//.userDetailsService(userDetailsService);
 
 	}
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JdbcTokenStore(dataSource);
 	}
 
 	@Bean
-	public TokenEnhancer jwtTokenEnhancer() {
+	public TokenEnhancer tokenEnhancer() {
 		return new CustomTokenEnhancer();
 	}
 
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("<signingKey>");
+		return accessTokenConverter;
+	}
+
+	@Bean
+	public DefaultTokenServices defaultTokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		defaultTokenServices.setSupportRefreshToken(true);
+		defaultTokenServices.setTokenEnhancer(tokenEnhancer());
+		return defaultTokenServices;
+	}
+	
 }
