bf3c8862e2125b6f958ea25b0132fe441d98b0a4
Satyam Kumar <skumar@texala.net>
2022-08-16 13:38:06 +0530

    sdf

--- src/main/java/com/satyam/security/service/config/AuthorizationServerConfig.java
+++ src/main/java/com/satyam/security/service/config/AuthorizationServerConfig.java
@@ -1,16 +1,15 @@
 package com.satyam.security.service.config;
 
-import java.util.Arrays;
-
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -18,12 +17,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * 
@@ -37,57 +32,36 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	@Autowired
-	private WebResponseExceptionTranslator<OAuth2Exception> loggingExceptionTranslator;
-	@Resource(name = "userDetailsServiceImpl")
-	private UserDetailsService userService;
+	private DataSource dataSource;
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private WebResponseExceptionTranslator<OAuth2Exception> loggingExceptionTranslator;
+	@Autowired
+	private RedisConnectionFactory connectionFactory;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("solartopps").secret(passwordEncoder.encode("Texala@1234"))
-				.accessTokenValiditySeconds(20000).refreshTokenValiditySeconds(20000).authorities("USER")
-				.autoApprove(true).authorizedGrantTypes("password", "refresh_token").scopes("read", "write");
-
+		clients.jdbc(dataSource);
 	}
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()")
+				.allowFormAuthenticationForClients();
 	}
+
+	@Resource(name = "userDetailsServiceImpl")
+	private UserDetailsService userDetailsService;
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
-		endpoints.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain)
-				.authenticationManager(authenticationManager);//.userDetailsService(userService);
+		endpoints.authenticationManager(authenticationManager)
+				.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST).tokenStore(redisTokenStore())
+				.tokenEnhancer(new CustomTokenEnhancer());
 	}
 
 	@Bean
-	public JwtAccessTokenConverter accessTokenConverter() {
-		CustomJwtAccessTokenConverter accessTokenConverter = new CustomJwtAccessTokenConverter();
-		accessTokenConverter.setSigningKey("a1b2c3d4e5f6g");
-		return accessTokenConverter;
-	}
-
-	@Bean
-	public TokenEnhancer tokenEnhancer() {
-		return new CustomTokenEnhancer();
-	}
-
-	@Bean
-	public TokenStore tokenStore() {
-		return new JwtTokenStore(accessTokenConverter());
-	}
-
-	@Bean
-	@Primary
-	public DefaultTokenServices tokenServices() {
-		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-		defaultTokenServices.setTokenStore(tokenStore());
-		defaultTokenServices.setSupportRefreshToken(true);
-		return defaultTokenServices;
+	public TokenStore redisTokenStore() {
+		return new RedisTokenStore(connectionFactory);
 	}
 
 }
